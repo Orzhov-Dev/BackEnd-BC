1668389721acfa62acefa25b4c4b03e375951ded
aLouis Home <alouis@stagiaire-humanbooster.com>
2024-02-24 03:25:35 +0100

    comments

--- src/Controller/Admin/Trait/ReadOnlyTrait.php
+++ src/Controller/Admin/Trait/ReadOnlyTrait.php
@@ -11,7 +11,7 @@ trait ReadOnlyTrait
     public function configureActions(Actions $actions) : Actions
     {
         $actions
-            ->disable(Action::NEW, Action::EDIT, Action::DELETE)
+            // ->disable(Action::NEW, Action::EDIT, Action::DELETE)
             ->add(Crud::PAGE_INDEX, Action::DETAIL);
 
         return $actions;
